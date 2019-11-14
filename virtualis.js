b42bfe02fbecdcf676337a9e81941d0317151782
Youcef B <ybena102@uottawa.ca>
2019-11-14 17:40:21 -0500

    Fixed bug

--- virtualis.js
+++ virtualis.js
@@ -1,7 +1,8 @@
 var Pictures = ["url('./Pictures/Wallpaper1.jpg')", "url('./Pictures/Wallpaper2.jpg')", "url('./Pictures/Wallpaper3.jpg')", "url('./Pictures/Wallpaper4.jpg')", "url('./Pictures/Wallpaper5.jpg')", "url('./Pictures/Wallpaper6.jpg')", "url('./Pictures/Wallpaper9.jpg')", "url('./Pictures/Wallpaper10.jpg')", "url('./Pictures/Wallpaper11.jpg')", "url('./Pictures/Wallpaper12.jpg')", "url('./Pictures/Wallpaper13.jpg')", "url('./Pictures/Wallpaper14.jpg')", "url('./Pictures/Wallpaper15.jpg')"]
-var nav = document.getElementById("Nav");
+
 
 function ChangeBg() {
+    var nav = document.getElementById("Nav");
     var current = nav.style.backgroundImage
     var rand = Pictures[Math.floor(Math.random() * Pictures.length)];
     while (rand == current) {
