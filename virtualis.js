279cfd124697fb1d434e423eeab1c6453fbf3c43
Youcef B <ybena102@uottawa.ca>
2019-11-14 17:37:37 -0500

    Made the javascript never pick the same background pic twice in a row. Starting services page

--- virtualis.js
+++ virtualis.js
@@ -1,13 +1,14 @@
 var Pictures = ["url('./Pictures/Wallpaper1.jpg')", "url('./Pictures/Wallpaper2.jpg')", "url('./Pictures/Wallpaper3.jpg')", "url('./Pictures/Wallpaper4.jpg')", "url('./Pictures/Wallpaper5.jpg')", "url('./Pictures/Wallpaper6.jpg')", "url('./Pictures/Wallpaper9.jpg')", "url('./Pictures/Wallpaper10.jpg')", "url('./Pictures/Wallpaper11.jpg')", "url('./Pictures/Wallpaper12.jpg')", "url('./Pictures/Wallpaper13.jpg')", "url('./Pictures/Wallpaper14.jpg')", "url('./Pictures/Wallpaper15.jpg')"]
-
+var nav = document.getElementById("Nav");
 
 function ChangeBg() {
-    var nav = document.getElementById("Nav");
+    var current = nav.style.backgroundImage
     var rand = Pictures[Math.floor(Math.random() * Pictures.length)];
+    while (rand == current) {
+        var rand = Pictures[Math.floor(Math.random() * Pictures.length)];s
+    }
     nav.style.transition="all 3s";
     nav.style.backgroundImage = rand;
-    /*document.body.style.transition = "all 3s";
-    document.body.style.backgroundImage = rand;*/
 }
 
 window.setInterval(function(){
