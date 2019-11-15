d2b913b301771eca492673d23480f8f1d852411e
Youcef B <ybena102@uottawa.ca>
2019-11-15 17:20:18 -0500

    Made a scroll up button.

--- virtualis.js
+++ virtualis.js
@@ -1,5 +1,18 @@
 var Pictures = ["url('./Pictures/Wallpaper1.jpg')", "url('./Pictures/Wallpaper2.jpg')", "url('./Pictures/Wallpaper3.jpg')", "url('./Pictures/Wallpaper4.jpg')", "url('./Pictures/Wallpaper5.jpg')", "url('./Pictures/Wallpaper6.jpg')", "url('./Pictures/Wallpaper9.jpg')", "url('./Pictures/Wallpaper10.jpg')", "url('./Pictures/Wallpaper11.jpg')", "url('./Pictures/Wallpaper12.jpg')", "url('./Pictures/Wallpaper13.jpg')", "url('./Pictures/Wallpaper14.jpg')", "url('./Pictures/Wallpaper15.jpg')"]
 
+window.onscroll = function() {BackToTop()};
+function BackToTop() {
+    var Top = document.getElementById("Scroll");
+    var vw = window.innerWidth;
+    var vh = window.innerHeight;
+  if (document.body.scrollTop > 0.40*vh || document.documentElement.scrollTop > 0.40*vh) {
+    Top.style.display = "block";
+  } else {
+    Top.style.display = "none";
+  }
+}
+
+
 
 function ChangeBg() {
     var nav = document.getElementById("Nav");
